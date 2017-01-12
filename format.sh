6fd4e4dd7100c01ef63d90b4a8d2888b49a424f2
Meinhard Ritscher <unreachable@gmx.net>
2017-01-12 09:31:34 +0100

    adding helper script for code style checks

---
+++ format.sh
@@ -0,0 +1,2 @@
+clang-format -i -style="{BasedOnStyle: llvm, ColumnLimit: 120, IndentWidth: 4, Standard: Cpp11, PointerBindsToType: false, BreakBeforeBraces: Linux, BreakConstructorInitializersBeforeComma: true, AccessModifierOffset: -4, BreakBeforeBinaryOperators: true}" $1
+
