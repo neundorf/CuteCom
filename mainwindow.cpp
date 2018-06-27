13f18e9ba1d611e4f99272592564c4db64f5b744
Jason Harrison <drjasonharrison@gmail.com>
2018-06-26 22:26:35 -0700

    Improve Logfilehandling (#65)
    by adding more flushes.
    Make sure logging goes to the file specified  when filename
    of logfile has been changed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -405,6 +405,7 @@ void MainWindow::closeDevice()
     m_command_history->setEnabled(false);
     if (m_logFile.isOpen()) {
         m_logFile.flush();
+        m_logFile.close();
     }
 }
 
@@ -452,12 +453,20 @@ void MainWindow::printDeviceInfo()
 
 void MainWindow::toggleLogging(bool start)
 {
-    if (m_logFile.isOpen() == start) {
+    QString currentLogFileName = m_lb_logfile->text();
+
+    if (m_logFile.isOpen() == start
+        && m_logFile.fileName() == currentLogFileName) {
         return;
     }
 
     if (start) {
-        m_logFile.setFileName(m_lb_logfile->text());
+        if (m_logFile.fileName() != currentLogFileName) {
+            m_logFile.flush();
+            m_logFile.close();
+            m_logFile.setFileName(m_lb_logfile->text());
+        }
+
         QIODevice::OpenMode mode = QIODevice::ReadWrite;
         mode = (controlPanel->m_check_appendLog->isChecked()) ? mode | QIODevice::Append : mode | QIODevice::Truncate;
 
@@ -1004,6 +1013,7 @@ void MainWindow::processData()
 
     if (m_logFile.isOpen()) {
         m_logFile.write(data);
+        m_logFile.flush();
     }
     m_output_display->displayData(data);
 }
